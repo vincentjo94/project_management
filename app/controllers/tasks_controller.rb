52441c5bc2c82e99f2bad48281b7ab5eb6972330
vincent jonathan <v.jonathan94@gmail.com>
2017-04-19 15:55:25 +0700

    Mark as complete ability for tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
 	before_action :set_project
+	before_action :set_task, except: [:create]
 
 	def create
 		@task = @project.tasks.create(task_params)
@@ -7,7 +8,6 @@ class TasksController < ApplicationController
 	end
 
 	def destroy
-		@task = @project.tasks.find(params[:id])
 		if @task.destroy
 			flash[:success] = "Task was deleted."
 		else
@@ -16,7 +16,16 @@ class TasksController < ApplicationController
 		redirect_to @project
 	end	
 
+	def complete
+		@task.update_attribute(:completed_at, Time.now)
+		redirect_to @project, notice: "Task Completed"
+	end	
+
 	private
+
+	def set_task
+		@task = @project.tasks.find(params[:id])
+	end	
 
 	def set_project
 		@project = Project.find(params[:project_id])
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170419063128) do
+ActiveRecord::Schema.define(version: 20170419080735) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "title"
@@ -22,8 +22,9 @@ ActiveRecord::Schema.define(version: 20170419063128) do
   create_table "tasks", force: :cascade do |t|
     t.string   "content"
     t.integer  "project_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.datetime "completed_at"
     t.index ["project_id"], name: "index_tasks_on_project_id"
   end
 
